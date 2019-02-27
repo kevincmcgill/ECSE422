0b0b5c5439949db30eca71af9b5351a87e1101d9
kevin Chen <willietanada@gmail.com>
2019-02-27 15:34:25 -0500

    init

--- project.py
+++ project.py
@@ -5,21 +5,38 @@ cost = [10, 25, 10, 20, 30, 10, 10, 25, 20, 20, 40, 10, 20, 10, 30]
 	
 
 def main():
-	for node in range(5):
-		temp = 0
-		for i in range(node + 1):
-			temp += i
+	# for node in range(node):
 
-		print ((node+1)*(numOfNodes-1)-temp-1)
+	nodesReli(6);
+	
 		
-main()
 
 
 def nodesReli(node):
-	
+
+	global numOfNodes
+	global reli
+	global cost
+		temp = 0
+	for i in range(node + 1):
+		temp += i
+
+		# print (node)
+		# print ("output" ,((node+1)*(numOfNodes-1)-temp-1)-numOfNodes + node + 1)
+	print("list:", end = ' ')
+
+	for k in range(3,-1,-1):
+
+		print( k, end = ' ')
+
+	print ()	
+	print ((node+1)*(numOfNodes-1)-temp-1)
+	print (((node+1)*(numOfNodes-1)-temp-1)-numOfNodes - node + 1)
+	for k in range(((node+1)*(numOfNodes-1)-temp-1),(((node+1)*(numOfNodes-1)-temp-1)-numOfNodes + node + 2), -1 ):
+		print ()	
 		
-		
-	
+main()
+
 
 	# public static double[] nodesReli(int node) {
 	# 	double [] nodeReli   = new double [numOfNodes-1];
