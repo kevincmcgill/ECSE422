063b5cc00c58af54a1825774597b1239bf6c9f88
Mei Qi Tang <mei.q.tang@mail.mcgill.ca>
2019-04-06 01:41:14 -0400

    inplement console user input output to multiple files

--- index.py
+++ index.py
@@ -1,19 +1,14 @@
-# import doubleList as DL
 import itertools
 
 # create by Mei Tang (260742835) and Kevin Chen (260658680) for ECSE 422 project
-# 
 
+# numOfNodes = 6
+# reliability = [0.94, 0.91, 0.96, 0.93, 0.92, 0.94, 0.97, 0.91, 0.92, 0.94, 0.90, 0.94, 0.93, 0.96, 0.91]
+# cost = [10, 25, 10, 20, 30, 10, 10, 25, 20, 20, 40, 10, 20, 10, 30]
+# if costGoal is negative, the cost constrain = infinity (no cost constaint)
+# costGoal = 0
+# reliabilityGoal = 0
 
-numOfNodes = 6
-reliability = [0.94, 0.91, 0.96, 0.93, 0.92, 0.94, 0.97, 0.91, 0.92, 0.94, 0.90, 0.94, 0.93, 0.96, 0.91]
-cost = [10, 25, 10, 20, 30, 10, 10, 25, 20, 20, 40, 10, 20, 10, 30]
-# if costGoal is negative, the cost constrain = infinity (no cost constain)
-costGoal = 100
-reliabilityGoal = 0.9
-
-
-edgeNum = len(reliability)
 class Edge:
 	def __init__(self,nodeA,nodeB,reliability,cost):
 		self.nodeA = nodeA
@@ -22,16 +17,32 @@ class Edge:
 		self.cost = cost
 
 
-# class Node:
-#     def __init__(self, data, prev, next):
-#         self.data = data
-#         self.prev = prev
-#         self.next = next
+def readInputFile(outputFileA, outputFileB, outputFileC):
+    lines = [line for line in open('input.txt') if not line.startswith('#') and
+             len(line.strip())]
+    numOfNodes = int(lines[0].split("\n")[0])
+    reliability = list(map(float, lines[1].split("\n")[0].split(" ")))
+    cost = list(map(float, lines[2].split("\n")[0].split(" ")))
+    edgeNum = len(reliability)
 
+    print("Number of Nodes:", numOfNodes)
+    print("Reliability Matrix:" , reliability)
+    print("Cost Matrix:" , cost)
+    print("Number of edges:", edgeNum)
+
+    for file in [outputFileA, outputFileB, outputFileC]:
+        outputFile = open(file, "w")
+        outputFile.write("Number of Nodes: " + str(numOfNodes) + "\n")
+        outputFile.write("Reliability Matrix: " + str(reliability) + "\n")
+        outputFile.write("Cost Matrix: " + str(cost) + "\n")
+        outputFile.write("Number of edges: " + str(edgeNum) + "\n")
+        outputFile.close()
+    return [numOfNodes, reliability, cost, edgeNum]
 
 
 def decreasingReli(elem):
     return elem.reliability
+
 
 def getCost(list):
     i = 0
@@ -40,19 +51,8 @@ def getCost(list):
         cost = cost + list[i].cost
     return cost
 
-# def buildSpanningTree(list):
-#     i = 0
-#     spanningTree = DL.DoubleList()
-#     for i in range(len(list)):
-#         if i == 0: #initial edge and nodes
-#             spanningTree.append(list[i].nodeA)
-#             spanningTree.append(list[i].nodeB)
-#             spanningTree.show()
-#         else:
-#             # add logic
-#             return
 
-def getReli(edge):
+def getReli(edge, numOfNodes):
     # to connect N nodes, we need at least N-1 edges
     # find all combination with N-1 edges (all kind of possible minimal spanning tree)
     combination = list(itertools.product([0, 1], repeat = len(edge)))
@@ -73,7 +73,7 @@ def getReli(edge):
             if combination[i][j] == 1: 
                 subGraph.append(edge[j])
                  
-        if not isAllConnected(subGraph):
+        if not isAllConnected(subGraph, numOfNodes):
             continue
 
 
@@ -89,10 +89,10 @@ def getReli(edge):
 
         outputReliability += subGraphReliability
 
-
     return outputReliability
 
-def isAllConnected(edge):
+
+def isAllConnected(edge, numOfNodes):
 
     # check if all is connected
     connectedNode = []
@@ -113,7 +113,8 @@ def isAllConnected(edge):
                 connectedNode.append(edge[x].nodeA)
         i += 1
 
-def buildSpanningTree(edge):
+
+def buildSpanningTree(edge, numOfNodes):
     connectedNode = []
     pickedEdge = []
     i = 0
@@ -122,7 +123,6 @@ def buildSpanningTree(edge):
             pickedEdge.append(edge[i])
             connectedNode.append(edge[i].nodeA)
             connectedNode.append(edge[i].nodeB)
-
 
         elif edge[i].nodeA in connectedNode and edge[i].nodeB in connectedNode:
             i += 1
@@ -138,7 +138,7 @@ def buildSpanningTree(edge):
     return pickedEdge
 
 
-def meetReliabilityGoal(edge):
+def meetReliabilityGoal(edge, numOfNodes, reliabilityGoal, costGoal, costContrained):
     
     # to connect N nodes, we need at least N-1 edges
     # find all combination with N-1 edges (all kind of possible minimal spanning tree)
@@ -165,33 +165,41 @@ def meetReliabilityGoal(edge):
             if combination[i][j] == 1: 
                 graph.append(edge[j])
                  
-        if not isAllConnected(graph):
+        if not isAllConnected(graph, numOfNodes):
             continue
 
-        if costGoal > 0:
+        if costContrained and costGoal > 0:
             if getCost(graph) > costGoal:
                 continue
 
-        if getReli(graph) >= reliabilityGoal:
+        if getReli(graph, numOfNodes) >= reliabilityGoal:
             graphSet.append(graph)
             # if len(graphSet) == 210:
             #       return graphSet
     return graphSet
 
-def findMaxReliability(graphSet):
+def findMaxReliability(outputFile, graphSet, numOfNodes):
     outputGraph = []
     MaxReli = 0
     for x in range(len(graphSet)):
-        if getReli(graphSet[x]) > MaxReli:
+        if getReli(graphSet[x], numOfNodes) > MaxReli:
             outputGraph = graphSet[x]
-            MaxReli = getReli(graphSet[x])
+            MaxReli = getReli(graphSet[x], numOfNodes)
 
-    print("")
+    outputFile.write("\n")
 
     for x in range(len(outputGraph)):
-        print("Edge #", x+1, ":", outputGraph[x].nodeA,"-", outputGraph[x].nodeB, "Reliability:",outputGraph[x].reliability, "Cost:", outputGraph[x].cost)
-    print("Total cost:", getCost(outputGraph))
-    print("Best Reliability", getReli(outputGraph)) 
+        outputFile.write("Edge # " + str(x+1) + " : " + str(outputGraph[x].nodeA) + " - " + str(outputGraph[x].nodeB) + " Reliability: " + str(outputGraph[x].reliability) + " Cost: " + str(outputGraph[x].cost) + "\n")
+    outputFile.write("Total cost: " + str(getCost(outputGraph)) + "\n")
+    outputFile.write("Max Reliability " + str(getReli(outputGraph,numOfNodes)) + "\n")
+
+def printSolutions(file,sol, numOfNodes):
+    for x in range(len(sol)):
+        file.write("Solution # " + str(x+1) + "\n")
+        file.write("Total reliability:" + str(getReli(sol[x], numOfNodes)) + "\n")
+        for i in range(len(sol[x])):
+            file.write("Edge # " + str(i+1) + ": " + str(sol[x][i].nodeA) + " - " + str(sol[x][i].nodeB) + " Reliability: " + str(sol[x][i].reliability) + " Cost: " + str(sol[x][i].cost) + "\n")
+        file.write("\n")
 
 
 # !!!!!!!!!! IMPORTANT !!!!!!!!!!!!
@@ -200,6 +208,18 @@ def findMaxReliability(graphSet):
 
 
 def main():
+    reliabilityGoal = float(input("Please enter reliability goal (from 0 to 1): "))
+    costGoal = int(input("Please enter cost constraint (from 1 to 100): "))
+    print(50 * "*")
+    print("Reliability goal set to: " + str(reliabilityGoal))
+    print("Cost constraint set to: " + str(costGoal))
+
+    inputValues = readInputFile("resultPartA.txt", "resultPartB.txt", "resultPartC.txt")
+    numOfNodes = inputValues[0]
+    reliability = inputValues[1]
+    cost = inputValues[2]
+    edgeNum = inputValues[3]
+
     edge = [None] * edgeNum
     z = 0
     # Create edge array
@@ -210,39 +230,28 @@ def main():
 
     edge.sort(key=decreasingReli, reverse=True)
 
-    # for x in range(edgeNum):
-    #     print("Edge #", x+1, ":", edge[x].nodeA,"-", edge[x].nodeB, "Reliability:",edge[x].reliability, "Cost:", edge[x].cost)
+    outputFileA = open("resultPartA.txt", "a")
+    outputFileA.write(50* "*" + "\n")
+    outputFileA.write("PART a) Solutions meeting reliability goal: " + str(reliabilityGoal) + "\n")
+    solA = meetReliabilityGoal(edge, numOfNodes, reliabilityGoal, costGoal, False)
+    printSolutions(outputFileA, solA, numOfNodes)
+    outputFileA.write(50 * "*" + "\n")
+    outputFileA.close()
 
-    # print("Total cost:", getCost(edge))
+    outputFileB = open("resultPartB.txt", "a")
+    outputFileB.write(50 * "*" + "\n")
+    outputFileB.write("PART b) Solutions meeting reliability goal: " + str(reliabilityGoal) + " given cost constraint: " + str(costGoal) + "\n")
+    solB = meetReliabilityGoal(edge, numOfNodes, reliabilityGoal, costGoal, True)
+    printSolutions(outputFileB, solB, numOfNodes)
+    outputFileB.write(50 * "*" + "\n")
+    outputFileB.close()
 
-    #build minimal spanning tree
-    # spanningTreeEdge = buildSpanningTree(edge)
-    # for x in range(len(spanningTreeEdge)):
-    #     print("Edge #", x+1, ":", spanningTreeEdge[x].nodeA,"-", spanningTreeEdge[x].nodeB, "Reliability:",spanningTreeEdge[x].reliability, "Cost:", spanningTreeEdge[x].cost)
-    # print("Total cost:", getCost(spanningTreeEdge))
-    
-
-
-    # all graph that meet the constrain
-    sol = meetReliabilityGoal(edge)
-    print(50* "*")
-    print("PART a) Solutions meeting reliability goal: ", reliabilityGoal, "\n")
-    # print out the entire set
-    for x in range(len(sol)):
-        print("Solution #",x+1)
-        print("Total reliability:",getReli(sol[x]))
-        for i in range(len(sol[x])):
-            print("Edge #", i+1, ":", sol[x][i].nodeA,"-", sol[x][i].nodeB, "Reliability:",sol[x][i].reliability, "Cost:", sol[x][i].cost)
-        print("")
-
-    print(50 * "*")
-    print("PART b) Solutions meeting reliability goal: ", reliabilityGoal, "given cost constraint:", costGoal, "\n")
-    print(50 * "*")
-    print("PART c) Solution with maximum reliability given cost constraint:", costGoal)
-    # find the max reliability in given constrain
-    findMaxReliability(sol)
-    print(50 * "*")
-    
+    outputFileC = open("resultPartC.txt", "a")
+    outputFileC.write(50 * "*" + "\n")
+    outputFileC.write("PART c) Solution for maximum reliability given cost constraint: " + str(costGoal) + "\n")
+    findMaxReliability(outputFileC, solB, numOfNodes)
+    outputFileC.write(50 * "*" + "\n")
+    outputFileC.close()
 
 
 if __name__ == "__main__":
